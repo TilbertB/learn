0e417c15fe47f720d799e66671ffaa44101cc2ad
Tilbert <tilbert.balaban.ki.2015@lpnu.ua>
2019-02-01 15:42:16 +0200

    add libs

--- Author Search (react+redux) (test taks)/author/src/App.js
+++ Author Search (react+redux) (test taks)/author/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import Main from './Main/main_container';
-
+import styled from 'styled-components';
+import { Container, Row, Col } from 'reactstrap';
 export default function App() {
+
+  const MainStyled = styled.div`
+    background-image: linear-gradient(#122c3f,#285f89);
+    height: 100vh;
+  `
+  // 122c3f - топ, 285f89 - бот
   return (
-    <div>
-      <Main/>  
-    </div>
+    <MainStyled>
+      <Container>
+        <Row>
+          <Col xs='5'>
+            <Main/> 
+          </Col>
+        </Row>
+      </Container>
+    </MainStyled>
   )
 }
